4be1df68e1e90241b62ea04b44ed88a6630732b4
wooki <hyunook02@naver.com>
2025-05-05 19:33:43 +0900

    chore(#1): Spring Boot 앱 생성

---
+++ src/test/java/kr/ac/ks/cs_web_back/CsWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.ks.cs_web_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsWebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

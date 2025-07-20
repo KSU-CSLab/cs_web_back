9bd50537fc191dc7164dbbfb175027ad7a8c1a80
wooki <hyunook02@naver.com>
2025-07-20 23:00:09 +0900

    feat(#5): 멤버 서비스 작성

---
+++ src/main/java/kr/ac/ks/cs_web_back/domain/Member/service/MemberService.java
@@ -0,0 +1,20 @@
+package kr.ac.ks.cs_web_back.domain.Member.service;
+
+import kr.ac.ks.cs_web_back.domain.Member.model.Member;
+import kr.ac.ks.cs_web_back.domain.Member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void createMember(String email, String password, String username, String birthdate, String number) {
+        Member member = Member.of(email, password, username, birthdate, number);
+        memberRepository.save(member);
+    }
+}

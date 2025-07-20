d4ecc29ee7e1b3723d61936fc03a47769fe1fc09
wooki <hyunook02@naver.com>
2025-07-20 22:36:50 +0900

    feat(#5): 회원가입 요청 바디 작성

---
+++ src/main/java/kr/ac/ks/cs_web_back/domain/Member/dto/request/MemberCreateRequest.java
@@ -0,0 +1,31 @@
+package kr.ac.ks.cs_web_back.domain.Member.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Schema(description = "회원가입")
+public record MemberCreateRequest(
+        @Schema(description = "이메일", example = "example@ks.ac.kr")
+        @NotBlank(message = "이메일이 입력되지 않았습니다.")
+        String email,
+
+        @Schema(description = "비밀번호", example = "examplePassword123")
+        @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
+        @Size(max = 24, message = "비밀번호는 24자 내로 입력해 주세요.")
+        String password,
+
+        @Schema(description = "사용자명", example = "userNickName")
+        @NotBlank(message = "사용자명이 입력되지 않았습니다.")
+        @Size(max = 12, message = "유저명은 12자 내로 입력해 주세요.")
+        String username,
+
+        @Schema(description = "생년월일", example = "2002.09.05")
+        @NotBlank(message = "생년월일이 입력되지 않았습니다.")
+        String birthdate,
+
+        @Schema(description = "전화번호", example = "055-663-5140")
+        @NotBlank(message = "전화번호가 입력되지 않았습니다.")
+        String number
+) {
+}

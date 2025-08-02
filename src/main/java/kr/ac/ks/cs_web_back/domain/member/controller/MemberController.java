b81ffa9979228b229f177494cac3a7bd5a0367bb
wooki <hyunook02@naver.com>
2025-08-02 16:32:41 +0900

    feat(#5): 회원가입 컨트롤러 리턴 타입 CsResponse 적용

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/MemberController.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/MemberController.java
@@ -1,14 +1,19 @@
 package kr.ac.ks.cs_web_back.domain.member.controller;
 
 import jakarta.validation.Valid;
+import kr.ac.ks.cs_web_back.domain.member.controller.code.MemberSuccessCode;
 import kr.ac.ks.cs_web_back.domain.member.dto.request.MemberCreateRequest;
 import kr.ac.ks.cs_web_back.domain.member.service.MemberService;
+import kr.ac.ks.cs_web_back.global.response.CsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/member")
@@ -18,10 +23,10 @@ public class MemberController implements SpringDocMemberController {
     private final MemberService memberService;
 
     @PostMapping("/register")
-    public ResponseEntity<Void> register(
+    public CsResponse<Long> register(
             @Valid @RequestBody MemberCreateRequest request
     ) {
-        memberService.createMember(request);
-        return ResponseEntity.ok().build();
+        Long id = memberService.createMember(request);
+        return CsResponse.of(MemberSuccessCode.GENERATED_REGISTERED, id);
     }
 }

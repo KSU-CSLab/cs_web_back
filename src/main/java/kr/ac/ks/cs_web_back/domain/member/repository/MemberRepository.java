60d93a14832bdfee513e08c120e209e2cd8630bf
wooki <hyunook02@naver.com>
2025-08-02 16:40:31 +0900

    feat(#5): 중복 이메일/닉네임 예외처리 및 멤버 팩토리메서드 변경

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/repository/MemberRepository.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import kr.ac.ks.cs_web_back.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }

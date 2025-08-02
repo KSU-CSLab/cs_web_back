b607d0b0505e27c8bdd1f6028b0f0c3624d94a9c
wooki <hyunook02@naver.com>
2025-08-02 16:54:30 +0900

    chore(#5): 스웨거 오류응답 예시 변경

--- src/main/java/kr/ac/ks/cs_web_back/global/swagger/error/ProblemDetailSchema.java
+++ src/main/java/kr/ac/ks/cs_web_back/global/swagger/error/ProblemDetailSchema.java
@@ -1,17 +1,13 @@
 package kr.ac.ks.cs_web_back.global.swagger.error;
 
 public record ProblemDetailSchema(
-        String type,
-        String title,
-        int status,
+        int code,
         String detail,
         String instance
 ) {
-    public static ProblemDetailSchema of(ApiErrorResponse apiErrorResponse, String detail) {
+    public static ProblemDetailSchema of(ApiErrorResponse apiErrorResponse, int code, String detail) {
         return new ProblemDetailSchema(
-                apiErrorResponse.type(),
-                apiErrorResponse.status().name(),
-                apiErrorResponse.status().value(),
+                code,
                 detail,
                 apiErrorResponse.instance()
         );

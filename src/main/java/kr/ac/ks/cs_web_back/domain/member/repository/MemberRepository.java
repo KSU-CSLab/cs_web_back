d4e03eca8e18490dbcc958de30384821dc13ad43
이건우 <dlrjsdn0518@gmail.com>
2025-08-11 22:51:31 +0900

    feat: 로그인 기능 기본 구조 구현 (#11)

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/repository/MemberRepository.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/repository/MemberRepository.java
@@ -3,7 +3,11 @@ package kr.ac.ks.cs_web_back.domain.member.repository;
 import kr.ac.ks.cs_web_back.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
+
+    Optional<Member> findByEmail(String email);
 }

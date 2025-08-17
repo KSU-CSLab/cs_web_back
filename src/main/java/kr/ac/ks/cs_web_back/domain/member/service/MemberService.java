4da060f2fde54298a081452a3cf100fd5f46afea
lgw323 <dlrjsdn0518@gmail.com>
2025-08-17 19:29:16 +0900

    refactor(#11): Member 도메인에서 로그인 관련 로직 제거

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/service/MemberService.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/service/MemberService.java
@@ -2,14 +2,14 @@ package kr.ac.ks.cs_web_back.domain.member.service;
 
 import kr.ac.ks.cs_web_back.domain.member.controller.code.MemberExceptionCode;
 import kr.ac.ks.cs_web_back.domain.member.dto.request.MemberCreateRequest;
-import kr.ac.ks.cs_web_back.domain.member.dto.request.MemberLoginRequest;
+import kr.ac.ks.cs_web_back.domain.auth.dto.request.AuthLoginRequest;
 import kr.ac.ks.cs_web_back.domain.member.model.Member;
 import kr.ac.ks.cs_web_back.domain.member.repository.MemberRepository;
 import kr.ac.ks.cs_web_back.global.exeption.domain.ConflictException;
 import kr.ac.ks.cs_web_back.global.exeption.domain.NotFoundException;
 import kr.ac.ks.cs_web_back.global.exeption.domain.UnauthorizedException;
+import kr.ac.ks.cs_web_back.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,12 +38,5 @@ public class MemberService {
                 .build();
 
         return memberRepository.save(member).getId();
-    }
-    public String loginMember(MemberLoginRequest request) {
-        Member member = memberRepository.findByEmail(request.email()).orElseThrow(() -> new NotFoundException(MemberExceptionCode.NOT_FOUND_USER));
-        if(!passwordEncoder.matches(request.password(), member.getPassword()))
-            throw new UnauthorizedException(MemberExceptionCode.UNAUTHORIZED_PASSWORD);
-        long tokenExpireTime = 1000L * 60 * 60;
-
     }
 }

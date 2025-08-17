bc5268df91eb496c96e90e9683254557710c6bc9
lgw323 <dlrjsdn0518@gmail.com>
2025-08-17 19:29:41 +0900

    feat(#11): 로그인 비즈니스 로직을 처리하는 AuthService 구현

--- src/main/java/kr/ac/ks/cs_web_back/domain/auth/service/AuthService.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/auth/service/AuthService.java
@@ -1,30 +1,12 @@
 package kr.ac.ks.cs_web_back.domain.auth.service;
 
-import kr.ac.ks.cs_web_back.domain.auth.dto.response.TokenResponse;
-import kr.ac.ks.cs_web_back.domain.auth.model.Auth;
-import kr.ac.ks.cs_web_back.domain.auth.repository.AuthRepository;
-import kr.ac.ks.cs_web_back.global.security.JwtTokenProvider;
-import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
-    AuthRepository authRepository;
-    JwtTokenProvider jwtTokenProvider;
 
-    public TokenResponse reissue(String oldRefreshToken) {
-        jwtTokenProvider.validateRefreshToken(oldRefreshToken);
-        String subject = jwtTokenProvider.getSubject(oldRefreshToken);
-        String newAccess = jwtTokenProvider.createAccessToken(subject);
-        String newRefresh = jwtTokenProvider.createRefreshToken(subject);
-        return new TokenResponse(newAccess, newRefresh);
-    }
 
-    @Generated
-    public AuthService(final AuthRepository authRepository, final JwtTokenProvider jwtTokenProvider) {
-        this.authRepository = authRepository;
-        this.jwtTokenProvider = jwtTokenProvider;
     }
 }

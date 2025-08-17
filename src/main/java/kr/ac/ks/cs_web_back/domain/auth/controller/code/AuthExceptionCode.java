2d77f1cc4fb5d5d163c614a86dce7886741d5334
lgw323 <dlrjsdn0518@gmail.com>
2025-08-17 19:30:20 +0900

    feat(#11): Auth 도메인 관련 Success/Exception 코드 정의

--- src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/code/AuthExceptionCode.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/code/AuthExceptionCode.java
@@ -1,31 +1,19 @@
 package kr.ac.ks.cs_web_back.domain.auth.controller.code;
 
 import kr.ac.ks.cs_web_back.global.exeption.dto.ExceptionCode;
-import lombok.Generated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum AuthExceptionCode implements ExceptionCode {
-    BAD_REQUEST_NO_REFRESH(9001, "토큰이 없습니다."),
-    UNAUTHORIZED_INVALID_FORMAT(8002, "유효하지 않은 토큰 형식입니다."),
-    UNAUTHORIZED_EXPIRED(8003, "만료된 토큰입니다."),
-    INTERNAL_SERVER_ERROR(5000, "서버에서 예기치 못한 오류가 발생했습니다.")
+
+    BAD_REQUEST_NO_EMAIL(9010, "이메일이 입력되지 않았습니다."),
+    BAD_REQUEST_NO_PASSWORD(9011, "비밀번호가 입력되지 않았습니다."),
+    UNAUTHORIZED_PASSWORD(8011, "비밀번호가 일치하지 않습니다."),
+    NOT_FOUND_USER(6001, "일치하는 사용자가 존재하지 않습니다."),
     ;
 
-    private int code;
-    private String message;
-
-    @Generated
-    public int getCode() {
-        return this.code;
-    }
-
-    @Generated
-    public String getMessage() {
-        return this.message;
-    }
-
-    @Generated
-    private AuthExceptionCode(final int code, final String message) {
-        this.code = code;
-        this.message = message;
-    }
+    private final int code;
+    private final String message;
 }
--- src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/code/AuthSuccessCode.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/code/AuthSuccessCode.java
@@ -1,29 +1,16 @@
 package kr.ac.ks.cs_web_back.domain.auth.controller.code;
 
 import kr.ac.ks.cs_web_back.global.response.SuccessCode;
-import lombok.Generated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum AuthSuccessCode implements SuccessCode {
-    OK_TOKEN_REISSUED(2005, "토큰 재발급에 성공했습니다."),
+
+    LOGIN_SUCCESS(2001, "로그인에 성공했습니다."),
     ;
 
-    private int code;
-    private String message;
-
-    @Generated
-    public int getCode() {
-        return this.code;
-    }
-
-    @Generated
-    public String getMessage() {
-        return this.message;
-    }
-
-    @Generated
-    private AuthSuccessCode(final int code, final String message) {
-        this.code = code;
-        this.message = message;
-    }
-
+    private final int code;
+    private final String message;
 }

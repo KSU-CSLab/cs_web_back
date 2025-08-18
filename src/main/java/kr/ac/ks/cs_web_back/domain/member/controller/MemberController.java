291d706b669002fdc0968f507e6bb60fc38d4023
myasehyun <qkrtpgus733@gmail.com>
2025-08-18 16:36:11 +0900

    feat(#13): 내정보 보기 컨트롤러 작성

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/MemberController.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/MemberController.java
@@ -3,10 +3,13 @@ package kr.ac.ks.cs_web_back.domain.member.controller;
 import jakarta.validation.Valid;
 import kr.ac.ks.cs_web_back.domain.member.controller.code.MemberSuccessCode;
 import kr.ac.ks.cs_web_back.domain.member.dto.request.MemberCreateRequest;
+import kr.ac.ks.cs_web_back.domain.member.dto.response.MemberResponse;
 import kr.ac.ks.cs_web_back.domain.member.service.MemberService;
 import kr.ac.ks.cs_web_back.global.response.CsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +26,10 @@ public class MemberController implements SpringDocMemberController {
     ) {
         Long id = memberService.createMember(request);
         return CsResponse.of(MemberSuccessCode.GENERATED_REGISTERED, id);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponse> getMemberInfo(@PathVariable Long id) {
+        return ResponseEntity.ok(memberService.getMemberInfo(id));
     }
 }

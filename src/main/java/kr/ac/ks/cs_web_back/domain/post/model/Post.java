2635405fc9db4a1e77a431ed6438f3585a270fcc
jiw00000 <alstj103126@naver.com>
2025-08-19 02:32:31 +0900

    feat(#15): post 엔티티 작성

---
+++ src/main/java/kr/ac/ks/cs_web_back/domain/post/model/Post.java
@@ -0,0 +1,50 @@
+package kr.ac.ks.cs_web_back.domain.post.model;
+
+import kr.ac.ks.cs_web_back.domain.member.model.Member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id",  nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ColumnDefault("0")
+    @Column(nullable = false)
+    private int commentCount;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    LocalDateTime createdAt;
+
+    public Post(Member member, String title, String content) {
+        this.member = member;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Post of(Member member, String title, String content) {
+        return new Post(member, title, content);
+    }
+}
+

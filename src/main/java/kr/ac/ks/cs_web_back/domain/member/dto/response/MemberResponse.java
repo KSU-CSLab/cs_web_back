98669255dd253addce76e6232a6cea230a8de687
myasehyun <qkrtpgus733@gmail.com>
2025-08-19 14:20:34 +0900

    feat(#13): 내정보 보기 	dto 작성

---
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/dto/response/MemberResponse.java
@@ -0,0 +1,23 @@
+package kr.ac.ks.cs_web_back.domain.member.dto.response;
+
+import kr.ac.ks.cs_web_back.domain.member.model.Member;
+
+import java.time.LocalDate;
+
+public record MemberResponse(
+        Long id,
+        String email,
+        String username,
+        LocalDate birthdate,
+        String number
+) {
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(
+                member.getId(),
+                member.getEmail(),
+                member.getUsername(),
+                member.getBirthdate(),
+                member.getNumber()
+        );
+    }
+}

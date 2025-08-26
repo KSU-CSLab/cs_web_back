93936c47a9cdfe6ce40e89660138a92bbed911f9
lgw323 <dlrjsdn0518@gmail.com>
2025-08-26 11:48:14 +0900

    feat(#11): 액세스/리프레시 토큰 발급 로직 구현

--- src/main/java/kr/ac/ks/cs_web_back/domain/auth/service/AuthService.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package kr.ac.ks.cs_web_back.domain.auth.service;
 
 import kr.ac.ks.cs_web_back.domain.auth.controller.code.AuthExceptionCode;
 import kr.ac.ks.cs_web_back.domain.auth.dto.request.AuthLoginRequest;
+import kr.ac.ks.cs_web_back.domain.auth.dto.response.AuthLoginResponse;
 import kr.ac.ks.cs_web_back.domain.member.model.Member;
 import kr.ac.ks.cs_web_back.domain.member.repository.MemberRepository;
 import kr.ac.ks.cs_web_back.global.exeption.domain.NotFoundException;
@@ -21,14 +22,20 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    public String loginMember(AuthLoginRequest request) {
+    public AuthLoginResponse loginMember(AuthLoginRequest request) {
         Member member = memberRepository.findByEmail(request.email())
                 .orElseThrow(() -> new NotFoundException(AuthExceptionCode.NOT_FOUND_USER));
 
         if(!passwordEncoder.matches(request.password(), member.getPassword()))
             throw new UnauthorizedException(AuthExceptionCode.UNAUTHORIZED_PASSWORD);
 
-        long tokenExpireTime = 1000L * 60 * 60;
-        return jwtUtil.generateToken(member.getEmail(), tokenExpireTime);
+        long accessTokenExpireTime = 1000L * 60 * 60; // 1 hour
+        long refreshTokenExpireTime = 1000L * 60 * 60;  // 24 hours
+        String accessToken = jwtUtil.generateToken(member.getEmail(), accessTokenExpireTime);
+        String refreshToken = jwtUtil.generateToken(member.getEmail(), refreshTokenExpireTime);
+        return AuthLoginResponse.builder()
+                .authorization(accessToken)
+                .authorizationRefresh(refreshToken)
+                .build();
     }
 }

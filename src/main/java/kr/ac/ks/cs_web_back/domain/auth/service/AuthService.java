71f848c1687dfe69a83d8a49901f09dbe730574b
lgw323 <dlrjsdn0518@gmail.com>
2025-08-26 11:48:14 +0900

    feat(#11): 액세스/리프레시 토큰 발급 로직 구현

--- src/main/java/kr/ac/ks/cs_web_back/domain/auth/service/AuthService.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/auth/service/AuthService.java
@@ -1,12 +1,41 @@
 package kr.ac.ks.cs_web_back.domain.auth.service;
 
+import kr.ac.ks.cs_web_back.domain.auth.controller.code.AuthExceptionCode;
+import kr.ac.ks.cs_web_back.domain.auth.dto.request.AuthLoginRequest;
+import kr.ac.ks.cs_web_back.domain.auth.dto.response.AuthLoginResponse;
+import kr.ac.ks.cs_web_back.domain.member.model.Member;
+import kr.ac.ks.cs_web_back.domain.member.repository.MemberRepository;
+import kr.ac.ks.cs_web_back.global.exeption.domain.NotFoundException;
+import kr.ac.ks.cs_web_back.global.exeption.domain.UnauthorizedException;
+import kr.ac.ks.cs_web_back.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AuthService {
 
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
+    public AuthLoginResponse loginMember(AuthLoginRequest request) {
+        Member member = memberRepository.findByEmail(request.email())
+                .orElseThrow(() -> new NotFoundException(AuthExceptionCode.NOT_FOUND_USER));
+
+        if(!passwordEncoder.matches(request.password(), member.getPassword()))
+            throw new UnauthorizedException(AuthExceptionCode.UNAUTHORIZED_PASSWORD);
+
+        long accessTokenExpireTime = 1000L * 60 * 60; // 1 hour
+        long refreshTokenExpireTime = 1000L * 60 * 60;  // 24 hours
+        String accessToken = jwtUtil.generateToken(member.getEmail(), accessTokenExpireTime);
+        String refreshToken = jwtUtil.generateToken(member.getEmail(), refreshTokenExpireTime);
+        return AuthLoginResponse.builder()
+                .authorization(accessToken)
+                .authorizationRefresh(refreshToken)
+                .build();
     }
 }

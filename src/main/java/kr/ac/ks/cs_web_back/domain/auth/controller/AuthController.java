65c5b8b1a124bea6135c095f0a24f7fa2ba0a6c0
lgw323 <dlrjsdn0518@gmail.com>
2025-08-26 11:48:27 +0900

    feat(#11): 로그인 API 응답 형식 변경

--- src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/AuthController.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package kr.ac.ks.cs_web_back.domain.auth.controller;
 import jakarta.validation.Valid;
 import kr.ac.ks.cs_web_back.domain.auth.controller.code.AuthSuccessCode;
 import kr.ac.ks.cs_web_back.domain.auth.dto.request.AuthLoginRequest;
+import kr.ac.ks.cs_web_back.domain.auth.dto.response.AuthLoginResponse;
 import kr.ac.ks.cs_web_back.domain.auth.service.AuthService;
 import kr.ac.ks.cs_web_back.global.response.CsResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +17,11 @@ public class AuthController implements SpringDocAuthController{
 
     private final AuthService authService;
 
-    @Override
     @PostMapping("/login")
-    @ResponseStatus(HttpStatus.OK)
-    public CsResponse<String> login(
+    public CsResponse<AuthLoginResponse> login(
             @Valid @RequestBody AuthLoginRequest request
     ) {
-        String token = authService.loginMember(request);
+        AuthLoginResponse token = authService.loginMember(request);
         return CsResponse.of(AuthSuccessCode.LOGIN_SUCCESS, token);
     }
 }

3fc01739cdfd35681e2a6bc85d1f32e2a619df3d
myasehyun <qkrtpgus733@gmail.com>
2025-09-10 16:45:50 +0900

    refactor(#13): MemberSuccessCode 명세서랑 다른 이름 수정

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/code/MemberSuccessCode.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/code/MemberSuccessCode.java
@@ -11,10 +11,11 @@ public enum MemberSuccessCode implements SuccessCode {
     GENERATED_REGISTERED(2101, "회원가입에 성공했습니다."),
     OK_WITHDRAWN(2102,"회원 탈퇴에 성공했습니다."),
     OK_FOUND_USER_PROFILE(2103, "유저 정보 조회에 성공했습니다."),
-    OK_MODIFIED_USER_PROFILE(2104, "유저 정보 변경에 성공했습니다."),
+    OK_MODIFIED_USER_PROFILE(2104, "사용자 정보 변경에 성공했습니다."),
     OK_FOUND_USER_WRITTEN_POSTS(2105, "해당 사용자가 작성한 게시글을 조회했습니다."),
     OK_FOUND_USER_WRITTEN_COMMENTS(2106, "해당 사용자가 작성한 댓글을 조회했습니다."),
     ;
+
 
     private final int code;
     private final String message;

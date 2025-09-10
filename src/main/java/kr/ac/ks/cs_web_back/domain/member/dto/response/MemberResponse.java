a31709742d0fb7a75edc51be026885cda16deb8b
myasehyun <qkrtpgus733@gmail.com>
2025-09-10 19:03:37 +0900

    fix(#13): MemberResponse에서 명세서에 맞게 id 필드 제거

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/dto/response/MemberResponse.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/dto/response/MemberResponse.java
@@ -5,7 +5,6 @@ import kr.ac.ks.cs_web_back.domain.member.model.Member;
 import java.time.LocalDate;
 
 public record MemberResponse(
-        Long id,
         String email,
         String nickname,
         LocalDate birthdate,
@@ -13,7 +12,6 @@ public record MemberResponse(
 ) {
     public static MemberResponse from(Member member) {
         return new MemberResponse(
-                member.getId(),
                 member.getEmail(),
                 member.getUsername(),
                 member.getBirthdate(),

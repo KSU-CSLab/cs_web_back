33c307a671cb4ab11fe8a9f60e9ec95a94436f6c
wooki <hyunook02@naver.com>
2025-09-18 01:24:35 +0900

    fix(#20): 명세서에 맞게 통일

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/dto/request/MemberCreateRequest.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/dto/request/MemberCreateRequest.java
@@ -12,12 +12,12 @@ public record MemberCreateRequest(
 
         @Schema(description = "비밀번호", example = "examplePassword123")
         @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
-        @Size(max = 24, message = "비밀번호는 24자 내로 입력해 주세요.")
+        @Size(max = 48, message = "비밀번호는 48자 이상일 수 없습니다.")
         String password,
 
         @Schema(description = "사용자명", example = "userNickName")
         @NotBlank(message = "사용자명이 입력되지 않았습니다.")
-        @Size(max = 12, message = "유저명은 12자 내로 입력해 주세요.")
+        @Size(max = 12, message = "사용자명은 12자 이상일 수 없습니다.")
         String username
 ) {
 }

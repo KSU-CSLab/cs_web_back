7335ba24b73edb8cd4668ff53e5d3f94fad1112a
wooki <hyunook02@naver.com>
2025-09-25 00:44:45 +0900

    컨트롤러 & dto & 서비스 구현 feat/#9

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/code/MemberExceptionCode.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/code/MemberExceptionCode.java
@@ -14,6 +14,7 @@ public enum MemberExceptionCode implements ExceptionCode {
     CONFLICT_EMAIL(5010, "이미 존재하는 이메일입니다."),
     CONFLICT_USERNAME(5012, "이미 존재하는 사용자명입니다."),
     NOT_FOUND_USER(6001, "일치하는 사용자가 존재하지 않습니다."),
+    UNAUTHORIZED_PASSWORD(8011, "비밀번호가 일치하지 않습니다."),
     ;
 
     private final int code;

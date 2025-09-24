52ff2da31376f6cb53247f64795870c9c9a81748
wooki <hyunook02@naver.com>
2025-09-25 01:00:33 +0900

    refactor(#9): 명세서에 맞게 코드 정리

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/code/MemberExceptionCode.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/code/MemberExceptionCode.java
@@ -9,11 +9,10 @@ import lombok.Getter;
 public enum MemberExceptionCode implements ExceptionCode {
 
     BAD_REQUEST_NO_EMAIL(9010, "이메일이 입력되지 않았습니다."),
-    BAD_REQUEST_NO_PASSWORD(9011, "비밀번호가 입력되지 않았습니다."),
-    BAD_REQUEST_NO_USERNAME(9012, "사용자명이 입력되지 않았습니다."),
-    CONFLICT_EMAIL(5010, "이미 존재하는 이메일입니다."),
+    BAD_REQUEST_NO_PASSWORD(9010, "비밀번호가 입력되지 않았습니다."),
+    BAD_REQUEST_NO_USERNAME(9010, "사용자명이 입력되지 않았습니다."),
+    CONFLICT_EMAIL(5010, "이미 가입된 이메일입니다."),
     CONFLICT_USERNAME(5012, "이미 존재하는 사용자명입니다."),
-    NOT_FOUND_USER(6001, "일치하는 사용자가 존재하지 않습니다."),
     UNAUTHORIZED_PASSWORD(8011, "비밀번호가 일치하지 않습니다."),
     ;
 

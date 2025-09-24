9a8c8207f3b17daea409e1547044e7155dc160fa
wooki <hyunook02@naver.com>
2025-09-25 01:06:47 +0900

    docs(#9): 회원탈퇴 문서 작성

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/model/Member.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/model/Member.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Collections;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter

7a2f368fbebd112bb692e55b2556fdd74c26de07
myasehyun <qkrtpgus733@gmail.com>
2025-09-30 11:59:30 +0900

    refactor(#13): 유저정보 조회 코드만 남김

--- src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/MemberController.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/member/controller/MemberController.java
@@ -1,14 +1,13 @@
 package kr.ac.ks.cs_web_back.domain.member.controller;
 
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.ac.ks.cs_web_back.domain.member.controller.code.MemberSuccessCode;
-import kr.ac.ks.cs_web_back.domain.member.dto.request.MemberCreateRequest;
 import kr.ac.ks.cs_web_back.domain.member.dto.response.MemberResponse;
 import kr.ac.ks.cs_web_back.domain.member.service.MemberService;
 import kr.ac.ks.cs_web_back.global.response.CsResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/member")
@@ -16,16 +15,6 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController implements SpringDocMemberInfoController {
 
     private final MemberService memberService;
-
-    @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public CsResponse<Long> register(
-            @Valid @RequestBody MemberCreateRequest request
-    ) {
-        Long id = memberService.createMember(request);
-        return CsResponse.of(MemberSuccessCode.GENERATED_REGISTERED, id);
-    }
-
     @GetMapping("/profile/{userid}")
     public CsResponse<MemberResponse> getMemberInfo(
             @RequestHeader("Authorization") String token,

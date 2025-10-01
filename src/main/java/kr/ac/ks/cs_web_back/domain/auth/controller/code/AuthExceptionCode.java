399a43d9a2bef61922c57a5b8d5f8cefbc770e81
이건우 <dlrjsdn0518@gmail.com>
2025-10-02 08:02:03 +0900

    refactor(#21): 미사용 예외 제거

--- src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/code/AuthExceptionCode.java
+++ src/main/java/kr/ac/ks/cs_web_back/domain/auth/controller/code/AuthExceptionCode.java
@@ -14,8 +14,6 @@ public enum AuthExceptionCode implements ExceptionCode {
     UNAUTHORIZED_PASSWORD(8011, "이메일 또는 비밀번호가 일치하지 않습니다."),
     UNAUTHORIZED_INVALID_TOKEN(8001, "유효하지 않은 토큰입니다."),
     UNAUTHORIZED_FAILED_VALIDATION(8002, "인증에 실패했습니다."),
-
-    INTERNAL_SERVER_ERROR(5000, "서버에서 예기치 못한 오류가 발생했습니다.")
     ;
 
     private final int code;
